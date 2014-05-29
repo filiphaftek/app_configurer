6f2728a0fd263cbe3617f11b02ff46bb8c7a9c6d
Filip Haftek <f.haftek@smartrecruiters.com>
2014-05-29 16:37:11 +0200

    Added handling many merge fields in one template value

--- app/configurer.js
+++ app/configurer.js
@@ -6,12 +6,18 @@ function configure(template){
     var templateFlat = flat.flatten(template)
     var environmentFlat = flat.flatten(envFile)
     for(obj in templateFlat){
-        if(templateFlat[obj].indexOf("{") > -1 && templateFlat[obj].indexOf("}") > -1){
-            var mergeField = templateFlat[obj].substring(templateFlat[obj].indexOf("{")+1,templateFlat[obj].indexOf("}"))
-            if(environmentFlat[mergeField]){
-                templateFlat[obj] = templateFlat[obj].replace("{" + mergeField + "}", environmentFlat[mergeField])
-            }else if(jsonPath.eval(envFile, mergeField)[0] instanceof Array) {
-                templateFlat[obj] =  jsonPath.eval(envFile, mergeField)[0]
+        while(templateFlat[obj].indexOf("{") > -1){
+            if(templateFlat[obj].indexOf("}") > -1){
+                var mergeField = templateFlat[obj].substring(templateFlat[obj].indexOf("{")+1,templateFlat[obj].indexOf("}"))
+                if(environmentFlat[mergeField]){
+                    templateFlat[obj] = templateFlat[obj].replace("{" + mergeField + "}", environmentFlat[mergeField])
+                }else if(jsonPath.eval(envFile, mergeField)[0] instanceof Array) {
+                    templateFlat[obj] =  jsonPath.eval(envFile, mergeField)[0]
+                }else{
+                    throw new Error("Environment must contain template field:" + mergeField)
+                }
+            }else{
+                throw new Error("Templates must contain merge fields in format:{fieldToMerge}")
             }
         }
     }
