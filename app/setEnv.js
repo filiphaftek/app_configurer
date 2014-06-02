90fa1d4bf86e25471aaa46c81689fd3e43b56a70
Filip Haftek <f.haftek@smartrecruiters.com>
2014-06-02 14:33:14 +0200

    Added setEnv

---
+++ app/setEnv.js
@@ -0,0 +1,21 @@
+/**
+ * Created by filip on 02/06/14.
+ */
+if(process.argv[2] === undefined){
+    throw new Error("Source file missing. Format: 'node setEnv.js sourceFile destinationFile'")
+}
+if(process.argv[3] === undefined){
+    throw new Error("Destination file missing. Format: 'node setEnv.js sourceFile destinationFile'")
+}
+
+var fs = require('fs');
+var configurer = require("../app/configurer");
+
+fs.readFile(process.argv[2], 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    var result = configurer.configure(JSON.parse(data));
+    console.log(result)
+    fs.writeFile(process.argv[3], JSON.stringify(result));
+});
